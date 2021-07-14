eedc20d2132d7c1965123af7a7e01290d8cf5d36
Yalincpp <yalinince8@gmail.com>
2021-07-14 20:32:11 +0300

    şifre değişimi eklendi

--- bilgisayar-v1.0.cpp
+++ bilgisayar-v1.0.cpp
@@ -4,50 +4,68 @@
 using namespace std;
 int main ()
 {
-    int secim;
+    int secim=-1;
     string parola;
     string dogruparola="dayimveben";
     string google;
     string alici;
     string verici;
     string posta;
+    string mparola;
+    string tparola;
+    string yparola;
 
-    for(int i=0; i<3 && parola != dogruparola; i++)
+    while(true)
     {
-
         cout << "BİR PAROLA GİRİN:" << endl;
         cin >> parola;
-    }
 
 
-    if(parola==dogruparola)
-    {
-        while(true)
+        if(parola==dogruparola)
         {
-            cout << "ÇIKIŞ:0, GOOGLE:1, GMAİL:2" << endl;
+            cout << "ÇIKIŞ:0, GOOGLE:1, GMAİL:2, PAROLA DEĞİŞTİRME:3" << endl;
             cin >> secim;
+        }
 
-            if(secim==1)
+        if(secim==1)
+        {
+            cout << "ARAMAK İÇİN YAZIN:" << endl;
+            cin >> google;
+        }
+
+        else if(secim==0)
+        {
+            break;
+        }
+
+        else if(secim==2)
+        {
+            cout << "ALICI E-POSTA ADRESİ:" << endl;
+            cin >> alici;
+            cout << "E-POSTA ADRESİNİZ:" << endl;
+            cin >> verici;
+            cout << "GÖNDERİNİZ:" << endl;
+            cin >> posta;
+
+        }
+        else if(secim==3)
+        {
+            cout << "MEVCUT PAROLA:" << endl;
+            cin >> mparola;
+            
+            if(mparola==dogruparola)
             {
-                cout << "ARAMAK İÇİN YAZIN:" << endl;
-                cin >> google;
+                cout << "YENİ PAROLA:" << endl;
+                cin >> yparola;
+                cout << "YENİ PAROLAYI ONAYLA:" << endl;
+                cin >> tparola;
+                
+                if(yparola==tparola)
+                {
+                    dogruparola=tparola;
+                    cout << "PAROLANIZ BAŞARIYLA DEĞİŞTİRİLDİ" << endl;
+                }
             }
-
-            else if(secim==0)
-            {
-                break;
-            }
-
-            else if(secim==2)
-            {
-                cout << "ALICI E-POSTA ADRESİ:" << endl;
-                cin >> alici;
-                cout << "E-POSTA ADRESİNİZ:" << endl;
-                cin >> verici;
-                cout << "GÖNDERİNİZ:" << endl;
-
-            }
-
         }
     }
 }
